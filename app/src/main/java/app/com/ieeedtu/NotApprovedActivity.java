23a2406b55b75596e230214063c831e54b2965b9
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-08-07 17:55:10 +0530

    UI Components added

---
+++ app/src/main/java/app/com/ieeedtu/NotApprovedActivity.java
@@ -0,0 +1,13 @@
+package app.com.ieeedtu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotApprovedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_not_approved);
+    }
+}
